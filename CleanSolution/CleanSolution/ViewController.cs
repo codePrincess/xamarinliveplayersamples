57253e63500fca5cfbdab46eeb69def1485fed46
Manu Rink <manuela.schrenk@gmail.com>
2017-05-30 16:42:23 +0200

    added clean single view xamarin.ios solution

---
+++ CleanSolution/CleanSolution/ViewController.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+using UIKit;
+
+namespace CleanSolution
+{
+    public class ViewController : UIViewController
+    {
+        public ViewController ()
+        {
+            Title = "ViewController";
+            View.BackgroundColor = UIColor.Red;
+        }
+
+        protected ViewController(IntPtr handle) : base(handle)
+        {
+            // Note: this .ctor should not contain any initialization logic.
+        }
+
+        public override void ViewDidLoad()
+        {
+            base.ViewDidLoad();
+            // Perform any additional setup after loading the view, typically from a nib.
+        }
+
+        public override void DidReceiveMemoryWarning()
+        {
+            base.DidReceiveMemoryWarning();
+            // Release any cached data, images, etc that aren't in use.
+        }
+    }
+}
